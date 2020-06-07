56172692acab3704dd6272d4e033755733ed9292
melissawelty <65548626+melissawelty@users.noreply.github.com>
2020-06-07 13:46:15 -0500

    issue a request

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,31 @@
 # app/robo_advisor.py
 
+import requests
+import json
+
+
+# INPUTS
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+
+response = requests.get(request_url)
+# print(type(response)) #> <class 'requests.models.Response'>
+# print(response.status_code) #> 200
+# print(response.text) #> dictionary object 
+
+parsed_response = json.loads(response.text)
+
+
+
+
+breakpoint()
+
+
+
+# OUTPUTS
+
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
